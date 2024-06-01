dd075e1656d19ec19d6904457e2d9b9596823f9a
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-06-01 19:59:46 +0300

    create the Add New Account UI

--- app/accounts.tsx
+++ app/accounts.tsx
@@ -1,7 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import AccountList from '@/components/AccountsList';
+import { useState } from 'react';
 
 export default function AccountScreen() {
+  const [name, setName] = useState('');
+  const [cap, setCap] = useState('');
+  const [tap, setTap] = useState('');
+
+  const createAccount = () => {
+    console.warn('Create account', name)
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -9,7 +19,16 @@ export default function AccountScreen() {
         <Text>CAP</Text>
         <Text>TAP</Text>
       </View>
+
       <AccountList />
+
+      <View style={styles.inputRow}>
+        <TextInput value={name} placeholder='Name' onChangeText={setName} style={styles.input} />
+        <TextInput value={cap} placeholder='CAP %' onChangeText={setCap} style={styles.input} />
+        <TextInput value={tap} placeholder='TAP %' onChangeText={setTap} style={styles.input} />
+      </View>
+
+      <Button title='Add account' onPress={createAccount} />
     </View>
   );
 };
@@ -25,4 +44,13 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     padding: 10,
   },
+  inputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  input: {
+    flex: 1
+  }
 });
